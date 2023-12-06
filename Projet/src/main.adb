aae3b39d4dafbf768dca88e328257429b42c3a18
Baptiste Des <146138426+baptdes@users.noreply.github.com>
2023-12-06 17:41:02 +0100

    Lecture fichier + calcule S

--- Projet/src/main.adb
+++ Projet/src/main.adb
@@ -9,6 +9,7 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 procedure Main is
    --Exceptions
       arguments_invalides : exception;
+      erreur_lecture_fichier : exception;
    
    --Variables
       --Valeur de alpha
@@ -26,28 +27,36 @@ procedure Main is
       i : Integer;
       -- Nom du fichier contenant le graphe
       nom_fichier : String;
+      -- Nombre de sites dans le graphe
+      nombre_site : Integer;
+      fichier : Ada.Text_IO.File_Type;
+      --Variables utlisé pour la lecture du fichier
+      a : Integer;
+      b : Integer;
+      -- Nombre de liaison d'un site
+      s : Integer;
 
 begin
    --Initialiser le programme
    
    --Initialiser les variables
-   alpha = 0.85;
-   k = 150;
-   epsilon = 0.0;
-   Prefix = "output";
-   Pleine = False;
+   alpha := 0.85;
+   k := 150;
+   epsilon := 0.0;
+   Prefix := "output";
+   Pleine := False;
 
    --Traiter la commande
-   i = 0;
+   i := 0;
    while (i<Argument_Count) loop
       begin
          case Argument(i) is
-            "-A" => alpha = float'Value(Argument(i+1)); i = i + 2;
-            "-K" => k = integer'Value(Argument(i+1)); i = i + 2;
-            "-E" => epsilon <- float'Value(Argument(i+1)); i = i + 2;
-            "-P" => i = i + 1;
-            "-C" => i = i + 1;
-            "-P" => Prefix = Argument(i+1); i = i + 2;
+            "-A" => alpha := float'Value(Argument(i+1)); i := i + 2;
+            "-K" => k := integer'Value(Argument(i+1)); i := i + 2;
+            "-E" => epsilon := float'Value(Argument(i+1)); i := i + 2;
+            "-P" => i := i + 1;
+            "-C" => i := i + 1;
+            "-P" => Prefix := Argument(i+1); i := i + 2;
          End Case;
          exception
             with others => raise arguments_invalides;
@@ -55,6 +64,45 @@ begin
       nom_fichier = Argument(Argument_Count);
 
    --Déterminer H grâce au fichier graphe
-   
+   open (fichier, In_File, nom_fichier);
+   Get (fichier, nombre_site);
+   S := Initialiser_matrice(nombre_site,nombre_site,0);
 
+   -- Calculer la matrice S
+	begin
+      -- Tant qu'il y a encore des valeurs à lire
+		while not End_Of_file (fichier) loop
+         -- Lire les deux prochaines valeurs
+			Get (fichier, a);
+         Get (fichier, b); 
+         -- Mettre un 1 dans la matrice pour signifier le lien
+         S(a+1,b+1) := 1;
+		end loop;
+	exception
+		when End_Error =>
+			null;
+		when Others =>
+         raise erreur_lecture_fichier;
+	end;
+
+   -- Pour chaque ligne
+   for i in 1..nombre_site loop
+      -- Si la ligne est vide
+      if ligne_vide(S,i) then
+         -- Remplir la ligne de 1/nombre de site
+         Modifier_ligne(S,i,1/nombre_site);
+      else
+         -- Calculer le nombre de liaison du site (i-1)
+         s := 0
+         for j in 1..nombre_site loop
+            s := s + S(i,j);
+         end loop;
+
+         -- Diviser la ligne par le nombre de liaison
+         for j in 1..nombre_site loop
+            S(i,j) := S(i,j) / s;
+         end loop;
+      end if;
+   end loop;
+   Close (fichier);
 end Main;
