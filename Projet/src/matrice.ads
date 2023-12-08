d6c97278efaa3bcd6da70488a0e9d26475c4125a
Aio13 <152897494+Aio13@users.noreply.github.com>
2023-12-08 11:02:48 +0100

    Update matrice.ads

--- Projet/src/matrice.ads
+++ Projet/src/matrice.ads
@@ -8,20 +8,29 @@ package matrice is
     Ligne_Hors_Bornes : exception;
     Taille_Differente_Addition: exception;
     Taille_Incompatible_Multiplication: exception;
+    Maximum_Indeterminable:exception;
     
     
     type T_mat is limited private;
     
     --Initialiser une matrice de taille l * c remplie de x
-    procedure Initialiser(l:in Integer; c:in Integer; M:out T_mat);
+    procedure Initialiser(l:in Integer; c:in Integer; x:in Float; M:out T_mat);
     
     --Est-ce que la ligne "ligne" dans la matrice M est vide ?
     --Renvoie l'exception Ligne_Hors_Bornes si ligne>nombre_ligne
     function Ligne_Vide (M :in T_mat; l:in Integer) return Boolean;
     
     --Enregistrer valeur à la case (ligne,colonne) dans la matrice M
-    --Renvoie l'Excepetion Case_Hors_Bornes si ligne>nombre_ligne ou si colonne>nombre_colonne
+    --Renvoie l'Excepetion Case_Hors_Bornes si ligne > M.nombre_ligne ou si colonne > M.nombre_colonne
     procedure Enregistrer(M:in out T_mat; ligne:in Integer; colonne:in Integer; valeur:in float);
+    
+    --Modifier toute une ligne d'une matrice M
+    --Renvoie l'exception Ligne_Hors_Bornes si ligne > M.nombre_ligne    
+    procedure Modifier_ligne(M:in out T_mat; ligne:in Integer; valeur:in float);
+    
+    --Renvoyer la ligne qui contient le maximum d'une /!\ MATRICE COLONNE /!\
+    --Renvoie l'exception Maximum_Indeterminable si la matrice n'est pas une matrice colonne
+    function Ligne_max(M:in T_mat) return integer;
     
     --Addition de deux matrices.
     --Renvoie l'exception Taille_Differente_Addition si les tailles ne correspondent pas
@@ -30,9 +39,15 @@ package matrice is
     --Multiplication de deux matrices (attention, on fait M1*M2 et non M2*M1)
     --Renvoie l'exception Taille_Incompatible_Multiplication si les dimenssions ne permettent pas la multiplication
     function Multiplication(M1:in T_mat; M2:in T_mat) return T_mat;
+    
+    
+    --Renvoyer la transposé d'une matrice
+    function Transpose(M:in T_mat) return T_mat with
+            Post => Result.nombre_ligne = M.nombre_colonne
+                    and Result.nombre_colonne = M.nombre_ligne;
             
 private
-    type T_Matrice is array(1..Capacite,1..Capacite);
+    type T_Matrice is array(1..Capacite,1..Capacite) of float;
     type T_mat is
         record
             nombre_ligne: Integer;
