26d379e97470ff7cbb01c0864f56ab77426f2fed
Aio13 <alexandre.montaldo@hotmail.fr>
2023-12-14 11:08:37 +0100

    Matrice VF

--- Projet/src/matrice.adb
+++ Projet/src/matrice.adb
@@ -1,24 +1,33 @@
-with Ada.Numerics.Elementary_Functions;
-use  Ada.Numerics.Elementary_Functions;
-
-package body matrice is
+package body Matrice is
     
     procedure Initialiser(l:in Integer; c:in Integer; x:in Float; M:out T_mat) is
     begin
-        for i in 1..l loop
-            for j in 1..c loop
-                M.Mat(i,j) := x;
+        --Si la taille est en dehors de la capacité on léve une exception
+        if l > Capacite or c > Capacite then
+            raise Taille_Hors_Capacite;
+        else
+            for i in 1..l loop
+                for j in 1..c loop
+                    M.Mat(i,j) := x;
+                end loop;
             end loop;
-        end loop;
-        M.nombre_colonne := c;
-        M.nombre_ligne := l;        
+            M.nombre_colonne := c;
+            M.nombre_ligne := l;
+        end if;
     end Initialiser;
     
     
-    --function Ligne_Vide (M :in T_mat; l:in Integer) is
-    --begin
-    --    none;
-    --end Ligne_vide;
+    function Ligne_Vide (M :in T_mat; l:in Integer) is
+    begin
+        for i in 1..M.nombre_colonne loop
+            if M.Mat(l,i) = 0 then
+                none;
+            else
+                return False;
+            end if;
+        end loop;
+        return True;
+    end Ligne_vide;
     
     
     procedure Enregistrer(M:in out T_mat; ligne:in Integer; colonne:in Integer; valeur:in float) is
@@ -41,45 +50,28 @@ package body matrice is
             raise Ligne_Hors_Bornes;
         end if;                
     end Modifier_ligne;
-
-    function Ligne_Vide (M :in T_mat; l:in Integer) return Boolean is
-        res : Boolean;
-    begin 
-        res := True;
-        for j in 1..M.nombre_colonne loop
-            if abs(M.Mat(l,j)) > 0.00001 then
-                res := False;
-            else
-                Null;
-            end if;
-        end loop;
-        return res;
-    end Ligne_Vide;
     
     
-    function Ligne_max(M:in T_mat) return integer is
-        maximum: Float;
-        indice : integer;
+    function Ligne_max(M:in T_mat) is
+        maximum: Integer;
     begin
         if M.nombre_colonne = 1 then
-            maximum := 0.0;
-            indice := 0;
+            maximum := 0;
             for i in 1..M.nombre_ligne loop
                 if M.Mat(i,1) >= maximum then
                     maximum := M.Mat(i,1);
-                    indice := i;
                 else
-                    Null;
+                    none;
                 end if;
             end loop;
-            return indice;
+            return maximum;
         else
             raise Maximum_Indeterminable;
-        end if;
+        end if;                
     end Ligne_max;    
     
     
-    function Addition(M1:in T_mat; M2:in T_mat) return T_mat is
+    function Addition(M1:in T_mat; M2:in T_mat) is
         Somme: T_mat;
     begin
         
@@ -98,20 +90,20 @@ package body matrice is
     end Addition;
     
     
-    function Multiplication(M1:in T_mat; M2:in T_mat) return T_mat is
+    function Multiplication(M1:in T_mat; M2:in T_mat) is
         Produit: T_mat;
-        s: Float;
+        s: Integer;
     begin
         if M1.nombre_colonne = M2.nombre_ligne then
             Produit.nombre_ligne := M1.nombre_ligne;
             Produit.nombre_colonne := M2.nombre_colonne;
             for i in 1..Produit.nombre_ligne loop
                 for j in 1..Produit.nombre_colonne loop
-                    s := 0.0;
+                    s := 0;
                     for k in 1..M1.nombre_colonne loop
-                        s := s + M1.Mat(i,k)*M2.Mat(k,j);
+                        s := s + M1(i,k)*M2(k,j);
                     end loop;
-                    Produit.Mat(i,j) := s;
+                    Produit(i,j) := s;
                 end loop;
             end loop;
             return Produit;
@@ -121,7 +113,7 @@ package body matrice is
     end Multiplication;
     
     
-    function Transpose(M:in T_mat) return T_mat is
+    function Transpose(M:in T_mat) is
         T : T_mat;
     begin
         T.nombre_colonne := M.nombre_ligne;
@@ -131,30 +123,20 @@ package body matrice is
                 T.Mat(j,i) := M.Mat(i,j);
             end loop;
         end loop;
-        return T;
     end Transpose;
     
-    function multiplier_scalaire (M:in T_mat;lambda:in Float) return T_mat is
-        T : T_mat;
+    
+    function Module(M:in T_mat) is
+        module: Integer;
     begin
-        T.nombre_colonne := M.nombre_ligne;
-        T.nombre_ligne := M.nombre_colonne;
-        for i in 1..M.nombre_ligne loop
-            for j in 1..M.nombre_colonne loop
-                T.Mat(i,j) := lambda * M.mat(i,j);
+        if M.nombre_colonne = 1 then
+            module := 0;
+            for i in 1..M.nombre_ligne loop
+                module := module + M.Mat(i,1);
             end loop;
-        end loop;
-        return T;
-    end multiplier_scalaire;
-
-    function norme (M:in T_mat) return Float is
-        s : Float;
-    begin
-        s := 0.0;
-        for i in 1..M.nombre_ligne loop
-            s := s + M.Mat(i,1)*M.Mat(i,1);
-        end loop;
-        return sqrt(s);
-    end norme;
-
-end matrice;
+        else
+            raise Module_Indeterminable;
+        end if;
+    end Module;
+        
+end Matrice;
--- Projet/src/matrice.ads
+++ Projet/src/matrice.ads
@@ -1,23 +1,22 @@
+with LCA;
+
 generic
     Capacite: Integer;
-package matrice is
+package Matrice is
     
+    Taille_Hors_Capacite : exception;
     Case_Hors_Bornes : exception;
     Ligne_Hors_Bornes : exception;
     Taille_Differente_Addition: exception;
     Taille_Incompatible_Multiplication: exception;
     Maximum_Indeterminable:exception;
+    Module_Indeterminable:exception;
     
-    type T_Matrice is array(1..Capacite,1..Capacite) of float;
-
-    type T_mat is
-        record
-            nombre_ligne: Integer;
-            nombre_colonne: Integer;
-            Mat: T_Matrice;
-        end record;
+    
+    type T_mat is limited private;
     
     --Initialiser une matrice de taille l * c remplie de x
+    --On léve l'eception Taille_Hors_Capacite si la taille demandée dépasse les capacités
     procedure Initialiser(l:in Integer; c:in Integer; x:in Float; M:out T_mat);
     
     --Est-ce que la ligne "ligne" dans la matrice M est vide ?
@@ -47,14 +46,20 @@ package matrice is
     
     --Renvoyer la transposé d'une matrice
     function Transpose(M:in T_mat) return T_mat with
-            Post => Transpose'Result.nombre_ligne = M.nombre_colonne
-                    and Transpose'Result.nombre_colonne = M.nombre_ligne;
-
-    --Multiplication par un scalaire
-    function multiplier_scalaire (M:in T_mat;lambda:in Float) return T_mat;
-
-    --Norme d'une matrice colone (vecteur)
-    function norme (M:in T_mat) return Float with
-        Pre => M.nombre_colonne = 1;
+            Post => Result.nombre_ligne = M.nombre_colonne
+            and Result.nombre_colonne = M.nombre_ligne;
+    
+    --Renvoie le module d'une matrice colonne
+    --Renvoie l'eception Module_Indeterminable si M n'est pas une matrice colonne
+    function Module(M:in T_mat) return Integer;
             
-end matrice;
+private
+    type T_Matrice is array(1..Capacite,1..Capacite) of float;
+    type T_mat is
+        record
+            nombre_ligne: Integer;
+            nombre_colonne: Integer;
+            Mat: T_Matrice;
+        end record;
+    
+end Matrice;
